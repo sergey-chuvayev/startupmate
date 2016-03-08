93a8acbb8e48d494fb3000602a5bec7be8ee0faf
Sergey <sergey@lookatme.ru>
2016-03-09 01:39:32 +0300

    check map

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,29 +1,41 @@
 $(function() {
+    function createSidebarLi(json){
+      return ("<li><a>" + json.name + "</a></li>");
+    };
+
+    function bindLiToMarker($li, marker){
+      $li.on('click', function(){
+        handler.getMap().setZoom(14);
+        marker.setMap(handler.getMap());
+        marker.panTo();
+        google.maps.event.trigger(marker.getServiceObject(), 'click');
+      })
+    };
+
+    function createSidebar(json_array){
+      _.each(json_array, function(json){
+        var $li = $( createSidebarLi(json) );
+        $li.appendTo('#sidebar_container');
+        bindLiToMarker($li, json.marker);
+      });
+    };
+
     handler = Gmaps.build('Google');
-handler.buildMap({
-    provider: {
-      disableDefaultUI: true
-      // pass in other Google Maps API options here
-    },
-    internal: {
-      id: 'map'
-    }
-  },
-  function(){
-    markers = handler.addMarkers([
-      {
-        "lat": 55.7252857,
-        "lng": 37.5734846,
-        "picture": {
-          "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
-          "width":  32,
-          "height": 32
-        },
-        "infowindow": "hello!"
-      }
-    ]);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-  }
-);
+    handler.buildMap({ internal: {id: 'sidebar_builder'}}, function(){
+      var json_array = [ // pass data from server
+        { lat: 40, lng: -80, name: 'Foo', infowindow: "I'm Foo" },
+        { lat: 45, lng: -90, name: 'Bar', infowindow: "I'm Bar" },
+        { lat: 50, lng: -85, name: 'Baz', infowindow: "I'm Baz" }
+      ];
+
+      var markers = handler.addMarkers(json_array);
+
+      _.each(json_array, function(json, index){
+        json.marker = markers[index];
+      });
+
+      createSidebar(json_array);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+    });
 });
